a1f1bc7348e852c3f71fd9f321964decb7b75167
juani83 <juanbecerra83@gmail.com>
2023-08-27 00:29:27 -0300

    Tipos de datos

---
+++ valores.js
@@ -0,0 +1,25 @@
+// PRIMITIVE
+
+// number
+40  
+
+// string
+"Diego De Granda"   
+
+// boolean
+true
+false
+
+// empty values / placeholder values
+null
+undefined
+
+
+// OBJECT
+
+// Array
+[1,2,3]
+["uno", "dos", "tres"]
+
+// Object
+{ nombre: "Diego" }
