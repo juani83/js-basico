45677d2d5de0bd5187e8f138342b113a84e44e29
juani83 <juanbecerra83@gmail.com>
2023-08-27 19:52:30 -0300

    Switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+var numero = 1;
+
+switch (numero) {
+    case 1: 
+        console.log("Soy uno");
+        break;
+    case 2: 
+        console.log("Soy dos");
+        break;
+    case 3: 
+        console.log("Soy tres");
+        break;
+    default:
+        console.log("Soy otra cosa");
+};
