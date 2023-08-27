6de275b706a39845e3fa1b35dc37d2e5af2fe42d
juani83 <juanbecerra83@gmail.com>
2023-08-27 13:41:51 -0300

    Variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Oscar";
+
+// declarar
+var edad;
+
+// inicializar
+edad = 30;
+
+edad;
+
+// declarar e inicializar
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Juan",
+    edad: 40
+};
+
+persona;
