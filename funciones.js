cdc9e6243dd9ccda04e696ac7d057563fff2173a
juani83 <juanbecerra83@gmail.com>
2023-08-27 14:35:01 -0300

    Funciones

---
+++ funciones.js
@@ -0,0 +1,13 @@
+// Declarativas
+function miFuncion() {
+    return 3;
+};
+
+miFuncion();
+
+//Expresion / Funciones anonimas
+var otraFuncion = function (a, b) {
+    return a + b;
+};
+
+otraFuncion(1,2);
